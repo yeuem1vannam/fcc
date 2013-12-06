1183e21c5d342fdfd73992ce378603c9dc01b8c6
Huy Nguyen <nguyen.dang.huy@framgia.com>
2013-12-06 09:14:48 +0700

    add total_score column in contest/user_scores

--- app/models/user_score.rb
+++ app/models/user_score.rb
@@ -4,6 +4,7 @@ class UserScore < ActiveRecord::Base
 
   scope :in_contest, ->contest_id{where(contest_id: contest_id) if contest_id}
   scope :of_user, ->user_id{where user_id: user_id}
+  default_scope ->{order 'point DESC'}
 
   validates :user_id, presence: true, numericality: true, uniqueness: {scope: :contest_id}
   validates :contest_id, presence: true, numericality: true
